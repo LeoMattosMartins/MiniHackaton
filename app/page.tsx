0a678812800a0180b6390f05bf8600f917afad42
[Dom Murphy] <dommurphy1229@gmail.com>
2023-10-07 13:17:13 -0400

    [Changing size of the logo]

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/FredtheDODO2.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={360}
+          height={74}
           priority
         />
       </div>
